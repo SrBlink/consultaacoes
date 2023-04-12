30ed23bec714cbc3314bface575d62518096df0e
SrBlink <thyago100cem@gmail.com>
2023-04-11 22:17:00 -0300

    consultando fundos-imobiliarios com fetch api

---
+++ src/interfaces/interfaces-requests.ts
@@ -0,0 +1,3 @@
+export interface GetAcoes{
+    sigla: string
+}
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,22 @@
-import fastify from "fastify";
+import fastify, { FastifyRequest, FastifyReply } from "fastify";
+import * as _serviceAcoes from "./services";
+import * as _types from "./interfaces/interfaces-requests";
 
 const app = fastify();
 
-app.get("/", (req, res) => {
-  res.send("Estamos ao vivassso!!!");
+app.get<{ Params: _types.GetAcoes }>("/sigla-bolsa-valores/:sigla", async (request, reply) => {
+  const { sigla } = request.params;
+
+  if (sigla) reply.status(200).send(await _serviceAcoes.ObterSiteAcoes(sigla));
+
+  reply.status(404).send({ error: "Sigla não foi encontrada" });
 });
 
-app.listen({
-  host: "0.0.0.0",
-  port: process.env.PORT ? Number(process.env.PORT) : 3333,
-}).then(() => {
-    console.log("HTTP Server ON!")
-});
+app
+  .listen({
+    host: "0.0.0.0",
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server ON!");
+  });
